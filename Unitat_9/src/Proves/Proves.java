61cf85ba71508d9c7d57ab865a9e48be1ba3f1ab
ciclesgs <ciclesgs@EAAULA211W-007>
2024-03-13 18:16:49 +0100

    primer commit ud9

---
+++ Unitat_9/src/Proves/Proves.java
@@ -0,0 +1,13 @@
+package Proves;
+
+public class Proves {
+
+	public static void main(String[] args) {
+		// llista: col·lecció de coses (variables, objectes).
+		// mapa: estructura que no s'accedeix amb un index (número), si no per altres vies (p.ex: string).
+		// set: conjunt d'objectes, sense ordre.
+		// 
+		
+	}
+
+}
