d9688ec9b5b5db300f21763b3df5d131a0185863
ciclesgs <ciclesgs@EAAULA211W-007>
2024-03-14 18:02:45 +0100

    commit de bckup

--- Unitat_8/src/Examen_UD7_UD8/Puntuacio.java
+++ Unitat_8/src/Examen_UD7_UD8/Puntuacio.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Examen_UD7_UD8;
 
 import java.time.LocalDate;
@@ -53,59 +52,3 @@ public class Puntuacio {
 	}
 	
 }
-=======
-package Examen_UD7_UD8;
-
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
-public class Puntuacio {
-
-	// Atributs.
-	private String nom;
-	private String cognoms;
-	private int punts;
-	private LocalDate data;
-	
-	private static DateTimeFormatter formatData = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-	
-	// Constructor.
-	public Puntuacio(String nom, String cognoms, int punts) {
-		this.nom = nom;
-		this.cognoms = cognoms;
-		this.punts = punts;
-		this.data = LocalDate.now();
-	}
-
-	// Getters i Setters.
-	public String getNom() {
-		return nom;
-	}
-
-	public String getCognoms() {
-		return cognoms;
-	}
-
-	public int getPunts() {
-		return punts;
-	}
-
-	public LocalDate getData() {
-		return data;
-	}
-	
-	// Mètode toString.
-	public String toString() {
-		StringBuffer sortida = new StringBuffer();
-		
-		sortida.append(IU.llistaOrdenada(false, 
-				"Nom: " + nom,
-				"Cognoms: " + cognoms,
-				"Punts: " + punts,
-				"Data: " + formatData.format(data)));
-		
-		return sortida.toString();
-	}
-	
-}
->>>>>>> refs/remotes/origin/Unitat_8
