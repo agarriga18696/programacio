7f61c207d1b2cec0875d2654b093fd45744a4ce7
agarriga18696 <agarriga18696@iesjoanramis.org>
2024-06-10 15:45:25 +0200

    * redirección del enlace del icono de la App.

--- src/vista/VentanaLogin.java
+++ src/vista/VentanaLogin.java
@@ -6,7 +6,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.FocusEvent;
 import java.awt.event.FocusListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
